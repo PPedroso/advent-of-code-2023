d5f86337dd7426fdf6717f92f27f1a9a1a95ca47
PPedroso <pedroso_pedro@hotmail.com>
2023-12-26 23:25:49 +0000

    finish day1 part 1

--- day1.cs
+++ day1.cs
@@ -1,8 +1,30 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+
 public static class Day1
 {
     public static void Process()
     {
+        StreamReader streamReader = new StreamReader("day1\\input1.txt");
 
+        string line = string.Empty;
+        var total = 0;
+        while (!streamReader.EndOfStream)
+        {
+            line = streamReader.ReadLine();
+            var filtered = line.Where(l => (int)l >= 48 && (int)l <= 57).ToList();
+            
+            var digit1 = filtered[0];
+            var digit2 = filtered.Count > 1 ? filtered[filtered.Count - 1] : filtered[0];
+            
+            var calValues = String.Concat(digit1, digit2);
+            total += int.Parse(calValues);
+            Console.WriteLine("value:" + calValues);
+        }
+
+        Console.WriteLine(total);
     }
 
 }
